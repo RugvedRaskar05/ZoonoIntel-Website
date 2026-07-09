6ef39bc60c59efb21ddcf6f945f010c62df3528a
RugvedRaskar05 <devidas.raskar4@gmail.com>
2026-07-09 17:54:02 -0400

    Create page.js

---
+++ app/figures/page.js
@@ -0,0 +1,8 @@
+export default function FiguresPage() {
+  return (
+    <main style={{ padding: "40px", color: "white" }}>
+      <h1>Figures</h1>
+      <p>Upload your project figures here later.</p>
+    </main>
+  );
+}
