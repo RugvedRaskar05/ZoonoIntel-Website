465263955530c3e4865e90cf60003147e87ca0b7
RugvedRaskar05 <devidas.raskar4@gmail.com>
2026-07-09 17:46:27 -0400

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,14 @@
+export default function AppPage() {
+  return (
+    <div style={{ height: "100vh", width: "100%", background: "#0A1A2F" }}>
+      <iframe
+        src="https://zoonointel-38b8.onrender.com"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+      />
+    </div>
+  );
+}
