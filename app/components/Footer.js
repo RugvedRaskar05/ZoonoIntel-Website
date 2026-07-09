6782b586ef04161a979c3244518049cf7e606978
RugvedRaskar05 <devidas.raskar4@gmail.com>
2026-07-09 18:01:22 -0400

    Create Footer.js

---
+++ app/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      marginTop: "60px",
+      padding: "20px",
+      fontSize: "14px",
+      color: "#00AEEF"
+    }}>
+      © 2026 ZoonoIntel | Built by Rugved Raskar
+    </footer>
+  );
+}
