d644e6e9ce2577053fbc57d74de16212f69e1dd1
RugvedRaskar05 <devidas.raskar4@gmail.com>
2026-07-15 18:53:23 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -50,5 +50,18 @@ export default function RootLayout({ children }) {
   );
 }
 
-<div className="cyber-bg"></div>
+<div className="particles">
+  {Array.from({ length: 40 }).map((_, i) => (
+    <div
+      key={i}
+      className="particle"
+      style={{
+        left: `${Math.random() * 100}%`,
+        top: `${Math.random() * 100}%`,
+        animationDelay: `${Math.random() * 10}s`,
+      }}
+    />
+  ))}
+</div>
+
 
