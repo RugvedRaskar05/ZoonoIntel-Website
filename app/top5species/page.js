1b4f8f166d5af619e554920a996101d967dce613
RugvedRaskar05 <devidas.raskar4@gmail.com>
2026-07-25 07:50:12 -0400

    Update page.js

--- app/top5species/page.js
+++ app/top5species/page.js
@@ -18,7 +18,7 @@ export default function FiguresPage() {
         {/* Red Fox */}
         <div>
           <img 
-            src="/public/red_fox.jpg" 
+            src="/images/red_fox.jpg" 
             alt="Red Fox" 
             style={{ width: "250px", borderRadius: "10px", marginBottom: "15px" }}
           />
@@ -34,7 +34,7 @@ export default function FiguresPage() {
         {/* Brown Bear */}
         <div>
           <img 
-            src="/public/brown_bear.jpg" 
+            src="/images/brown_bear.jpg" 
             alt="Brown Bear" 
             style={{ width: "250px", borderRadius: "10px", marginBottom: "15px" }}
           />
@@ -49,7 +49,7 @@ export default function FiguresPage() {
         {/* Leopard */}
         <div>
           <img 
-            src="/public/leopard.jpg" 
+            src="/images/leopard.jpg" 
             alt="Leopard" 
             style={{ width: "250px", borderRadius: "10px", marginBottom: "15px" }}
           />
@@ -64,7 +64,7 @@ export default function FiguresPage() {
         {/* Grey Wolf */}
         <div>
           <img 
-            src="/public/grey_wolf.jpg" 
+            src="/images/grey_wolf.jpg" 
             alt="Grey Wolf" 
             style={{ width: "250px", borderRadius: "10px", marginBottom: "15px" }}
           />
@@ -79,7 +79,7 @@ export default function FiguresPage() {
         {/* Coyote */}
         <div>
           <img 
-            src="/public/coyote.jpg" 
+            src="/images/coyote.jpg" 
             alt="Coyote" 
             style={{ width: "250px", borderRadius: "10px", marginBottom: "15px" }}
           />
