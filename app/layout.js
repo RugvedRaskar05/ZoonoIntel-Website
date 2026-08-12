4e6444afaf9e72504f11cdb73130eda0e818761f
RugvedRaskar05 <devidas.raskar4@gmail.com>
2026-08-12 17:15:33 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,12 @@
 import './globals.css';
 import Footer from '../components/Footer';
 
+<body>
+  <div className="page-transition">
+    {children}
+  </div>
+</body>
+
 export const metadata = {
   title: 'Zoonointel',
   description: 'The AI-powered zoonotic resevoir potential intelligence platform.',
