0f39ac53e6c6242ce9ced0cfce1179965899cbf8
RugvedRaskar05 <devidas.raskar4@gmail.com>
2026-08-26 17:50:38 -0400

    Update page.js

--- app/app/page.js
+++ app/app/page.js
@@ -2,7 +2,7 @@ export default function AppPage() {
   return (
     <div style={{ height: "100vh", width: "100%", background: "#0A1A2F" }}>
       <iframe
-        src="https://zoonointel-ne7d.onrender.com"
+        src="https://zoonointel-ne7d.onrender.com/?__theme=dark"
         style={{
           width: "100%",
           height: "100%",
